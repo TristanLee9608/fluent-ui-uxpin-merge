c59b18daab3b9cdb9b5b42b6b29aa22c5b1999d9
unknown <uklee@tangunsoft.com>
2021-10-25 17:05:27 +0900

    update button presets

--- src/components/Button/presets/0-default.jsx
+++ src/components/Button/presets/0-default.jsx
@@ -9,5 +9,5 @@ import Button from '../Button';
  */
 
 export default (
-  <Button uxpId="button1" style={{width: '100%'}} />
+  <Button uxpId="button1" style={{width: '100%', innerHeight: '30px'}} />
 );
