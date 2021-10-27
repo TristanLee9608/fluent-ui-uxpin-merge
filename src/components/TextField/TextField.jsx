ec0924385a3b22c14a92a75b48cf1560e80aa29d
unknown <uklee@tangunsoft.com>
2021-10-27 11:40:41 +0900

    update textfield

--- src/components/TextField/TextField.jsx
+++ src/components/TextField/TextField.jsx
@@ -3,22 +3,14 @@ import PropTypes from 'prop-types';
 import StyledInput from './TextField.styles';
 import { Position } from '@fluentui/react/lib/utilities/positioning';
 
-const posTop = "top";
-const posStart = "start";
-const posEnd = "end";
-
 export default class TextField extends PureComponent {
+  
   static propTypes = {
     label: PropTypes.string,
     /**
      * @uxpindescription Description label of the SpinButton
      * @uxpinpropname Label
      * @uxpincontroltype textfield(2)
-     * */
-    labelDisplay: PropTypes.oneOf([posStart, posTop, posEnd]),
-    /**
-     * @uxpindescription Whether the label appears on the left or on top
-     * @uxpinpropname Position
      * */
     width: PropTypes.oneOf(['stretched', 'auto']),
     onChange: PropTypes.func,
@@ -30,7 +22,6 @@ export default class TextField extends PureComponent {
 
   static defaultProps = {
     label: 'Basic Text Field',
-    labelDisplay: posStart,
     width: 'stretched',
     onChange: () => undefined,
     value: '',
@@ -38,14 +29,8 @@ export default class TextField extends PureComponent {
   };
 
   render() {
-
-    let iconProps = { iconName: this.props.iconName }
-    let position = this.props.labelDisplay === posEnd ? Position.end :
-        this.props.labelDisplay === posTop ? Position.top :
-            Position.start;
-
     return (
-    <StyledInput {...this.props} labelPosition={position} type="text" />
+    <StyledInput {...this.props} labelPosition='start' type="text" />
     );
   }
 }
