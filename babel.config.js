723e49542255fd22839b4a04221321d5d0e40799
unknown <uklee@tangunsoft.com>
2021-10-27 09:27:04 +0900

    update babel config

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
 module.exports= {
-  plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/plugin-syntax-dynamic-import'],
+  plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/plugin-syntax-dynamic-import', '@babel/plugin-proposal-class-properties'],
   presets: ['@babel/preset-env', '@babel/preset-react']
 };
