151d056d169c369bb5bea4a446da6f5d411f3de6
unknown <uklee@tangunsoft.com>
2021-10-28 09:02:42 +0900

    revert textfield

--- src/components/TextField/presets/0-default.jsx
+++ src/components/TextField/presets/0-default.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import * as React from 'react';
 import TextField from '../TextField';
 
+
+
 export default (
-  <TextField uxpId="1" />
+    <TextField uxpId="textfield1"/>
 );
